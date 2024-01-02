be5e7a58a085cc7c9b3994f7f40e7e9ee28548e6
rod608 <rod608ney@gmail.com>
2024-01-02 00:59:15 -0400

    Updated contact section to link to linkedin and included resume

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { srConfig, email } from '@config';
+import { srConfig, socialMedia } from '@config';
 import sr from '@utils/sr';
 import { usePrefersReducedMotion } from '@hooks';
 
@@ -60,11 +60,12 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I’m currently looking for entry-level software engineering opportunities as I am expected to
+        graduate by this December, 2024. Whether you have a question or just want to say hi, I’ll
+        try my best to get back to you!
       </p>
 
-      <a className="email-link" href={`mailto:${email}`}>
+      <a className="email-link" href={`${socialMedia[1]['url']}`}>
         Say Hello
       </a>
     </StyledContactSection>
